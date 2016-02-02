8f48956f63329aa678c1f4b9f2da73f27fd714e8
anisaifah <anisaifah@gmail.com>
2016-02-03 01:56:47 +0700

    Update SmartPhone.java

--- Exercise_1/SmartPhone.java
+++ Exercise_1/SmartPhone.java
@@ -1,1 +1,30 @@
-
+//1103130239, Anisa Nur Arifah, IF-38-01
+public class Smartphone{
+	private int totalApp;
+	private int memory;
+	private Application[] appList = new Application[10];
+	
+	public void setMemory(int memory){
+		this.memory = memory;
+	}
+	
+	public int getRemainingSize(){
+		int temp=0;
+		for (int i=0; i<appList.length;i++){
+			if(appList[i] != null){
+				temp = temp + appList[i].getAppSize();
+			}
+		}
+		return memory-temp;
+	}
+	public void addApplication(AppStore appStore, int appId){
+		Application App = appStore.getApp(appId);
+		if (App.getAppSize() <= getRemainingSize()){
+			appList[totalApp] = App;
+			totalApp++;
+		}		
+	}
+	public String toString(){
+		return ("Memory size "+memory+"MB, "+totalApp+" Application installed, remaining memory size : "+getRemainingSize()+ "MB");
+	}
+}
