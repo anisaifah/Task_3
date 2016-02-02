23581447194101e420e5bfa2825c9eab79b74198
anisaifah <anisaifah@gmail.com>
2016-02-03 01:55:00 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,17 @@
-
+//11013130239, Anisa Nur Arifah, IF-38-01
+public class AppStore{
+	private int totalApp;
+	private Application[] appList = new Application[100];
+	
+	public void createNewApp(String appName, int appSize){
+		Application App = new Application(appName, appSize);
+		appList[totalApp] = App;
+		totalApp++;
+	}
+	public Application getApp(int id){
+		return appList[id];
+	}
+	public String toString(){
+		return (" There are "+totalApp+ "applications ready to install");
+	}
+}
