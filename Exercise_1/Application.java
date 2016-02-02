87d3f9f41e255ecd6f52dc270bb75aa7f924a68a
anisaifah <anisaifah@gmail.com>
2016-02-03 01:55:49 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,20 @@
-
+//1103130239, Anisa Nur Arifah, IF-38-01
+public class Application{
+	private String appName;
+	private int appSize;
+	
+	public Application(String appName, int appSize){
+		this.appName = appName;
+		this.appSize = appSize;
+	}
+	
+	public String getAppName(){
+		return appName;
+	}
+	public int getAppSize(){
+		return appSize;
+	}
+	public String toString(){
+		return("Application Name : "+appName+", with size : "+appSize+ "MB");
+	}
+}
